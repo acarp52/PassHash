f5a3bcba03fe6acab4a1189b55c8493a1c12bd20
Andrew Carpenter <acarp52@gmail.com>
2017-03-18 19:53:32 -0400

    Added gitignore

--- test.sh
+++ test.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-HASH1=`curl -s -X POST --data "password=angryMonkey" http://localhost:8080 &`
-HASH2=`curl -s -X POST --data "password=angryMonkeyy" http://localhost:8080 &`
-HASH3=`curl -s -X POST --data "password=angryMonkeyyy" http://localhost:8080 &`
+PW1=`curl -s -X POST --data "password=angryMonkey" http://localhost:8080`
+PW2=`curl -s -X POST --data "password=testPassword" http://localhost:8080`
+PW3=`curl -s -X POST --data "password=foobar" http://localhost:8080`
+HASH1 = "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
+HASH2 = "iluLRhHe5Gs9rzUx+rsqc6k6K+N26qJA3BFd1YGL0kpTPu7ppGqqJ8gGRRbkieYLdVM1Bud04ZeSKEKMkQrydQ=="
+HASH3 = "ClAmHr0aOQ/tK/Mm8mc8FFWCpjQtUjIElz0CGTN/gWFqgGmwElh89WNfaSXxtWw2AjDBmyc1AO4BPgMGAb8kJQ=="
 
+if [ $PW1!=$HASH1 ]
+then
+	echo "Problem with HASH1"
+elif [ $PW2!=$HASH2 ]
+then
+	echo "Problem with HASH2"
+elif [ $PW3!=$HASH3 ]
+then
+	echo "Problem with HASH3"
+else
+	echo "All HASH tests passed."
+fi
 
 CONCURRENT=$(
-curl -s -X POST --data "password=angryMonkey1" http://localhost:8080 &
-curl -s -X POST --data "password=angryMonkey2" http://localhost:8080 &
-curl -s -X POST --data "password=angryMonkey3" http://localhost:8080 &
-curl -s -X POST --data "password=angryMonkey4" http://localhost:8080 &
-curl -s -X POST --data "password=angryMonkey5" http://localhost:8080 &
-curl -s -X POST --data "password=angryMonkey6" http://localhost:8080 &
-curl -s -X POST --data "password=angryMonkey7" http://localhost:8080 &
-curl -s -X POST --data "password=angryMonkey8" http://localhost:8080 &
-curl -s -X POST --data "password=angryMonkey9" http://localhost:8080 &
+curl -s -X POST --data "password=foobar1" http://localhost:8080 &
+curl -s -X POST --data "password=foobar2" http://localhost:8080 &
+curl -s -X POST --data "password=foobar3" http://localhost:8080 &
+curl -s -X POST --data "password=foobar4" http://localhost:8080 &
+curl -s -X POST --data "password=foobar5" http://localhost:8080 &
+curl -s -X POST --data "password=foobar6" http://localhost:8080 &
+curl -s -X POST --data "password=foobar7" http://localhost:8080 &
+curl -s -X POST --data "password=foobar8" http://localhost:8080 &
+curl -s -X POST --data "password=foobar9" http://localhost:8080 &
 )
 
 wait
